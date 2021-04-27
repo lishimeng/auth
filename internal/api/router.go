16ddd07e9d88cb2cdfade93653bd581ffff2e471
lishimeng <316527907@qq.com>
2021-04-27 15:45:35 +0800

    api interface

--- internal/api/router.go
+++ internal/api/router.go
@@ -3,14 +3,24 @@ package api
 import (
 	"github.com/kataras/iris/v12"
 	"github.com/lishimeng/auth/internal/api/tokenApi"
+	"github.com/lishimeng/auth/internal/api/userApi"
 )
 
 func Route(app *iris.Application) {
-	p := app.Party("/tokenApi")
-	token(p)
+	route(app.Party("/api"))
+}
+
+func route(root iris.Party) {
+	token(root.Party("/token"))
+	user(root.Party("/user"))
 }
 
 func token(p iris.Party) {
 	p.Post("/gen", tokenApi.GenToken)
 	p.Get("/verify", tokenApi.VerifyToken)
+}
+
+func user(p iris.Party) {
+	p.Post("/sign_in", userApi.SignIn)
+	p.Post("/logout", userApi.Logout)
 }
--- internal/api/userApi/user.api.go
+++ internal/api/userApi/user.api.go
@@ -2,6 +2,20 @@ package userApi
 
 import "github.com/kataras/iris/v12"
 
-func Login(ctx iris.Context) {
+type Req struct {
+	LoginName string `json:"loginName,omitempty"`
+	Password string `json:"password,omitempty"`
+}
 
+func SignIn(ctx iris.Context) {
+
+	// TODO 校验用户
+	// TODO 创建jwt
+	// TODO 返回jwt, role_list
+	// TODO role_list 存入cache
+	// TODO jwt 存入cache
+}
+
+func Logout(ctx iris.Context) {
+	ctx.GetHeader("")
 }
